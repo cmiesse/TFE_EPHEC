23fb8aa60ab224ac48d893dc334785fddb6d1461
cmiesse <c.miesse@students.ephec.be>
2020-03-21 16:06:07 +0100

    Partie Node register et login

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
-const express = require("express");
-const mysql = require("mysql");
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+var port = process.env.PORT || 5000;
 
-const app = express();
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "Jslmdpmlrdc3419$",
-  database: "tfe"
-});
-
-connection.connect(err => {
-  if (err) {
-    return err;
-  }
-  console.log("connecté");
-});
-
-app.get("/api/users", (req, res) => {
-  const SELECT_ALL_DENREES_QUERY = "SELECT * FROM utilisateurs";
-  connection.query(SELECT_ALL_DENREES_QUERY, (err, results) => {
-    if (err) {
-      return res.send(err);
-    } else {
-      return res.json({ data: results });
-    }
-  });
-});
-
-const port = 5000;
-app.listen(port, () =>
-  console.log(`Le serveur a commencé sur le port ${port}`)
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
+
+var Users = require("./routes/Users");
+
+app.use("/users", Users);
+
+app.listen(port, function() {
+  console.log("Server is running on port: " + port);
+});
