69ab701f8639753139331fbae5812a6895333172
cmiesse <c.miesse@students.ephec.be>
2020-03-26 18:16:54 +0100

    Ajout de méthodes GET pour toutes les autres tables dans l'API

--- server.js
+++ server.js
@@ -60,6 +60,93 @@ app.get("/api/annonces/:id", (req, res) => {
   });
 });
 
+// Obtenir toutes les denrées
+app.get("/api/denrees", (req, res) => {
+  const SELECT_ALL_DENREES_QUERY = "SELECT * from denrees";
+  connection.query(SELECT_ALL_DENREES_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir la denrée avec l'id donné
+app.get("/api/denrees/:id", (req, res) => {
+  const DenreeID = req.params.id;
+  const SELECT_DENREE_BY_ID_QUERY = `SELECT * from denrees WHERE DenreeID=${DenreeID}`;
+  connection.query(SELECT_DENREE_BY_ID_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir tous les types de denrée
+app.get("/api/types", (req, res) => {
+  const SELECT_ALL_TYPES_QUERY = "SELECT * from types";
+  connection.query(SELECT_ALL_TYPES_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir le type de denrée avec l'id donné
+app.get("/api/types/:id", (req, res) => {
+  const TypeID = req.params.id;
+  const SELECT_TYPE_BY_ID_QUERY = `SELECT * from types WHERE TypeID=${TypeID}`;
+  connection.query(SELECT_TYPE_BY_ID_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir tous les magasins
+app.get("/api/magasins", (req, res) => {
+  const SELECT_ALL_MAGASINS_QUERY = "SELECT * from magasins";
+  connection.query(SELECT_ALL_MAGASINS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir le magasin avec l'id donné
+app.get("/api/magasins/:id", (req, res) => {
+  const MagasinID = req.params.id;
+  const SELECT_MAGASIN_BY_ID_QUERY = `SELECT * from magasins WHERE MagasinID=${MagasinID}`;
+  connection.query(SELECT_MAGASIN_BY_ID_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
 app.listen(port, function() {
   console.log("Server is running on port: " + port);
 });
