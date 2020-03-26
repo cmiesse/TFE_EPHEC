3ff99bf711be15505859bbf891672ebd9b5f561f
cmiesse <c.miesse@students.ephec.be>
2020-03-26 11:20:18 +0100

    Ajout api annonces

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 5000;
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Jslmdpmlrdc3419$",
+  database: "tfe"
+});
+
+// Connexion à la DB
+connection.connect(err => {
+  if (err) {
+    return err;
+  }
+  console.log("DB : Connecté");
+});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,9 +27,38 @@ app.use(
   })
 );
 
+//Retravailler partie utilisateurs
 var Users = require("./routes/Users");
-
 app.use("/users", Users);
+
+// Obtenir toutes les annonces
+app.get("/api/annonces", (req, res) => {
+  const SELECT_ALL_ANNONCES_QUERY = "SELECT * from annonces";
+  connection.query(SELECT_ALL_ANNONCES_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+// Obtenir l'annonce avec l'id donné
+app.get("/api/annonces/:id", (req, res) => {
+  const AnnonceID = req.params.id;
+  const SELECT_ANNONCE_BY_ID_QUERY = `SELECT * from annonces WHERE AnnonceID=${AnnonceID}`;
+  connection.query(SELECT_ANNONCE_BY_ID_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
 
 app.listen(port, function() {
   console.log("Server is running on port: " + port);
