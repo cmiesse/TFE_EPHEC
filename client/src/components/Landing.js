3384b5455683e5c2fdc688cf0329c3ca5f40abb7
cmiesse <c.miesse@students.ephec.be>
2020-05-02 12:09:08 +0200

    userLocation (début) + selection rayon

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,103 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import "../App.css";
 
 class Landing extends Component {
   constructor() {
     super();
     this.state = {
+      UtilisateurID: "",
+      userLongitude: "",
+      userLatitude: "",
       Annonces: [],
+      rayon: 20,
     };
   }
-
   getAnnonces() {
     fetch(`/api/annoncesLanding`)
       .then((res) => res.json())
       .then((res) => this.setState({ Annonces: res.data }))
       .catch((err) => console.log(err));
   }
+  getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position, options = { enableHighAccuracy: true }) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        console.log(`La précision est de ${position.coords.accuracy} mètres.`);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        this.setState({ userLatitude: latitude });
+        this.setState({ userLongitude: longitude });
+      }
+    );
+  };
+  getUserTypes(user) {
+    fetch(`/api/userTypes/${user}`)
+      .then((res) => res.json())
+      .then((res) => this.setState({ Types: res.data }))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  getUserDenrees(user) {
+    fetch(`/api/userDenrees/${user}`)
+      .then((res) => res.json())
+      .then((res) => this.setState({ Denrees: res.data }))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  getUser() {
+    const token = localStorage.usertoken;
+    const decoded = jwt_decode(token);
+    this.setState({
+      UtilisateurID: decoded.UtilisateurID,
+    });
+  }
+
   componentDidMount() {
+    /*if (sessionStorage.usertoken !== "") {
+      setTimeout(() => {
+        this.getUser();
+      }, 1);
+    }
+    */
     this.getAnnonces();
   }
   render() {
     return (
       <div className="container">
         <h1>Page d'accueil</h1>
+        <div>
+          <input
+            type="button"
+            onClick={this.getUserLocation}
+            value={"Ma position"}
+            id="positionButton"
+          />
+        </div>
+        <div className="rayon">
+          <label className="labelRayon" htmlFor="rayon">
+            Choisissez le rayon de recherche :
+          </label>
+          <select
+            id="rayon"
+            value={this.state.rayon}
+            onChange={(e) =>
+              this.setState({
+                rayon: e.target.value,
+              })
+            }
+          >
+            <option value="5">5 km</option>
+            <option value="10">10 km</option>
+            <option value="20">20 km</option>
+          </select>
+        </div>
+
         <table align="center">
           <thead>
             <tr>
