03f9aa7739fb8150d50d180c94dfdfa7839d2f34
cmiesse <c.miesse@students.ephec.be>
2020-06-01 22:03:36 +0200

    Enseignes magasin + AnnonceDetails magasins logo

--- client/src/components/AnnonceDetails.js
+++ client/src/components/AnnonceDetails.js
@@ -3,6 +3,13 @@ import "../App.css";
 import Red from "./signs/iconeRouge.png";
 import Yellow from "./signs/iconeJaune.png";
 import Green from "./signs/iconeVert.png";
+import Aldi from "./signs/Aldi.png";
+import Carrefour from "./signs/Carrefour.png";
+import Cora from "./signs/Cora.png";
+import Colruyt from "./signs/Colruyt.png";
+import Delhaize from "./signs/Delhaize.png";
+import Lidl from "./signs/Lidl.png";
+import Spar from "./signs/Spar.png";
 
 class AnnonceDetails extends Component {
   constructor() {
@@ -35,6 +42,26 @@ class AnnonceDetails extends Component {
     }
   }
 
+  changeLogoMagasin(magasin) {
+    if (magasin.includes("Aldi")) {
+      return <img className="LogoMagasin" src={Aldi} alt="" />;
+    } else if (magasin.includes("Carrefour")) {
+      return <img className="LogoMagasin" src={Carrefour} alt="" />;
+    } else if (magasin.includes("Cora")) {
+      return <img className="LogoMagasin" src={Cora} alt="" />;
+    } else if (magasin.includes("Colruyt")) {
+      return <img className="LogoMagasin" src={Colruyt} alt="" />;
+    } else if (magasin.includes("Delhaize")) {
+      return <img className="LogoMagasin" src={Delhaize} alt="" />;
+    } else if (magasin.includes("Lidl")) {
+      return <img className="LogoMagasin" src={Lidl} alt="" />;
+    } else if (magasin.includes("Spar")) {
+      return <img className="LogoMagasin" src={Spar} alt="" />;
+    } else {
+      return "";
+    }
+  }
+
   componentDidMount() {
     this.changeTime(this.props.heures, this.props.minutes);
   }
@@ -43,7 +70,10 @@ class AnnonceDetails extends Component {
     return (
       <div className="Annonce">
         <b style={{ fontSize: 20 }}>{this.props.denree}</b>
-        <p>{this.props.magasin}</p>
+        <p>
+          {this.changeLogoMagasin(this.props.magasin)}
+          {this.props.magasin}
+        </p>
         {this.changeSign(this.props.quantite)}
         <p>Dernière modification : {this.state.temps}</p>
       </div>
